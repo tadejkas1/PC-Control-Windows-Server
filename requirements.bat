54fb4d17c6cfb43e1f6ff53293d368a19cc8bb30
TaDejKas <tadejkastelic5@gmail.com>
2024-01-16 13:42:04 +0100

    Update and rename run.bat to requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -2,6 +2,6 @@
 echo Do you want to install requirements.txt
 pause
 echo Installing required packages...
-pip install -r requirements.txt
+pip install -r requirements.txt --user
 echo Installation complete.
 pause
